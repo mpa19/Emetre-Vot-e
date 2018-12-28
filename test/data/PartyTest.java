4398de4cca9aef6f28cf4b4f6829be5e311e788e
mpa19 <marcperezarnaiz@gmail.com>
2018-12-28 15:20:42 +0100

    Check test party ok!

---
+++ test/data/PartyTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import org.junit.Test;
+
+
+/**
+ *
+ * @author Marc
+ */
+public class PartyTest {
+    
+    
+    public PartyTest() throws Exception {
+
+    }
+    
+    @Test
+    public void Test1() throws Exception{
+        Party party1 = new Party(null);
+        party1.getName();
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
