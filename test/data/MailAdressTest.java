49ddcdb9ae8e074c330dc5d7d6096013fb04cbe8
mpa19 <marcperezarnaiz@gmail.com>
2018-12-28 15:33:06 +0100

    Check test MailADress ok!

---
+++ test/data/MailAdressTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Marc
+ */
+public class MailAdressTest {
+    
+    public MailAdressTest() {
+    }
+    
+    @Test
+    public void Test2() throws Exception{
+        MailAdress mail1 = new MailAdress("marcperezarnaiz@gmail.com");
+        System.out.println(mail1.getMail());
+     
+    }
+    
+    @Test
+    public void Test1() throws Exception{
+        MailAdress mail1 = new MailAdress(null);
+    }
+    
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
